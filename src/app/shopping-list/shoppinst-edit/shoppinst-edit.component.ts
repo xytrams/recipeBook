09fa2602ff3feed3001915fde2798cd9246a7c6c
pevargasg <pevargas@valdosta.edu>
2017-11-13 00:03:50 -0500

    created all the main components for the app

---
+++ src/app/shopping-list/shoppinst-edit/shoppinst-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shoppinst-edit',
+  templateUrl: './shoppinst-edit.component.html',
+  styleUrls: ['./shoppinst-edit.component.css']
+})
+export class ShoppinstEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
