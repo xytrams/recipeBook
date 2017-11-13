57ffe06196ca3afc8bbe831341f81225d0f556f1
pevargasg <pevargas@valdosta.edu>
2017-11-13 01:17:17 -0500

    displaying recipies

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe}from '../recipe.module';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [] ;
+  recipes: Recipe[] = [
+    new Recipe(
+      'Taco',
+      'Pork Taco',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/001_Tacos_de_carnitas%2C_carne_asada_y_al_pastor.jpg/1200px-001_Tacos_de_carnitas%2C_carne_asada_y_al_pastor.jpg'
+    ),
+    new Recipe(
+      'Burrito',
+      'Pork Taco',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/001_Tacos_de_carnitas%2C_carne_asada_y_al_pastor.jpg/1200px-001_Tacos_de_carnitas%2C_carne_asada_y_al_pastor.jpg'
+    )
+  ] ;
   constructor() { }
 
   ngOnInit() {
